fe72ea4e96db9d198764af1f23834159d5be79c6
Annabelle Feldman <afeldman2025@kehillahstudent.org>
2025-03-24 10:30:35 -0700

    q3 done

--- q3.py
+++ q3.py
@@ -1,7 +1,15 @@
 def average(numbers):
     "Return the average of a list of numbers"
     # YOUR CODE HERE
-    return
+    # add up all the numbers
+    total=0 
+    for number in numbers: 
+        total = total + number
+    # find out how many numbers there are 
+    length=len (numbers)
+    # divide the total  by the number of numbers
+     
+    return total/length  
 
 print(average([32, 78, 48, 71, 93, 71, 79, 44, 5, 42])) #56.3
 print(average([5, 4, 3, 2, 1])) # 3.0
